74efa058eaee220d1ac468333f6399119f67461c
Sourabh Singh <145104933+Graphical27@users.noreply.github.com>
2025-07-09 23:57:56 +0530

    updated

---
+++ B_Fair_Numbers.cpp
@@ -0,0 +1,61 @@
+
+/*⠀⠀⠀⠀
+ Moon, tell me if I could
+Send up my heart to you?
+So, when I die, which I must do
+Could it shine down here with you?
+… 'Cause my love is mine, all mine
+I love mine, mine, mine
+Nothing in the world belongs to me
+But my love, mine, all mine, all mine
+… My baby here on Earth
+Showed me what my heart was worth
+So, when it comes to be my turn
+Could you shine it down here for her?
+*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD = 1e9 + 7;
+#define ln "\n";
+#define all(v) v.begin(),v.end()
+#define Fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int gcd(int a,int b){if(a==0) return b; return gcd(b%a,a);}
+int lcm(int a,int b){ return (a*b)/gcd(a,b);}
+int LCsubstring(string a, string b){int n = a.size(), m = b.size(), res = 0;vector<vector<int>> dp(n+1, vector<int>(m+1, 0));for(int i = 1; i <= n; i++){for(int j = 1; j <= m; j++){if(a[i-1] == b[j-1]){dp[i][j] = 1 + dp[i-1][j-1];res = max(res, dp[i][j]);}}}return res;}
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+
+void solve(){
+    int n; cin >> n;
+    
+    auto good = [&](int n){
+        int m = n;
+        while (m != 0){
+            int x = m % 10;
+            if (x != 0 && n % x != 0) return false;
+            m /= 10;
+        }return true;
+    };
+    while (!good(n))
+        n++;
+    
+    
+    cout << n << "\n";
+}
+
+int32_t main(){
+    auto begin = std::chrono::high_resolution_clock::now();
+    Fast_IO;
+    int t = 1; cin >> t;
+    while (t--){
+        solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+}
