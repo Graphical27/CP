66d8874cde10baabe7fda9aac41a9d11d17ce8c8
Sourabh Singh <specious0027@gmail.com>
2025-09-15 23:23:11 +0530

    updated

--- t_2.cpp
+++ t_2.cpp
@@ -25,12 +25,28 @@ using namespace std;
 // }
 
 class Solution {
-  public:
-    int findNumberOfComponent(int V, vector<vector<int>> &edges) {
-        vector<unordered_set<int>> v;
-        for(int i = 0; i < V; i++){
-          auto temp = eges[i];
-          
+public:
+    void dfs(int node, vector<vector<int>> &adj, vector<bool> &visited) {
+        visited[node] = true;
+        for (int neigh : adj[node]) {
+            if (!visited[neigh]) dfs(neigh, adj, visited);
         }
+    }
+    int findNumberOfComponent(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);  
+        }
+
+        vector<bool> visited(V, false);
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i, adj, visited);
+            }
+        }
+        return count;
     }
 };
