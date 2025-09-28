a5e81a0d10cdf25ec36a984561dfd03ea14966b2
Sourabh Singh <specious0027@gmail.com>
2025-09-28 23:58:26 +0530

    updated

--- t_2.cpp
+++ t_2.cpp
@@ -394,3 +394,60 @@ public:
         }return ans;
     }
 };
+
+
+
+
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        if(n == 0) return;
+        int m = board[0].size();
+        
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        
+        // check first & last row
+        for(int j = 0; j < m; j++){
+            if(board[0][j] == 'O' && !vis[0][j]){
+                q.push({0,j}); vis[0][j] = 1;
+            }
+            if(board[n-1][j] == 'O' && !vis[n-1][j]){
+                q.push({n-1,j}); vis[n-1][j] = 1;
+            }
+        }
+        // check first & last col
+        for(int i = 0; i < n; i++){
+            if(board[i][0] == 'O' && !vis[i][0]){
+                q.push({i,0}); vis[i][0] = 1;
+            }
+            if(board[i][m-1] == 'O' && !vis[i][m-1]){
+                q.push({i,m-1}); vis[i][m-1] = 1;
+            }
+        }
+        
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        
+        while(!q.empty()){
+            auto [r,c] = q.front(); q.pop();
+            board[r][c] = '#'; 
+            for(int k = 0; k < 4; k++){
+                int nr = r + dr[k], nc = c + dc[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && board[nr][nc]=='O' && !vis[nr][nc]){
+                    q.push({nr,nc});
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
+        
+     
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+    }
+};
