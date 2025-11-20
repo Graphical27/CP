01319b75f647442305c2577fee04c23128dbc57e
Sourabh Singh <specious0027@gmail.com>
2025-11-21 00:31:30 +0530

    updated

--- tt.cpp
+++ tt.cpp
@@ -756,6 +756,26 @@ public:
 	}
 };
 
+class Solution {
+public:
+	vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int> dist(V,INT_MAX);
+        for(int i = 0; i < V - 1; i++){
+            for(auto[u,v,w]:edges){
+                if(dist[u]!=INT_MAX && dist[v] > dist[u] + w){
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }for(auto[u,v,w]:edges){
+            if(dist[u] != INT_MAX && dist[v] > dist[u] + w){
+                return {-1};
+            }
+        }return dist;
+	}
+};
+
+
+
 
 // Multiple Source Shortest Path and detect negative cycle
 
