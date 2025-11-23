5e82702d9f21fd70738c88bb69a920a5db2b5a01
Sourabh Singh <specious0027@gmail.com>
2025-11-23 14:53:58 +0530

    Updated

--- tt.cpp
+++ tt.cpp
@@ -997,3 +997,34 @@ public:
         }return dist[end] == INT_MAX ? -1 : dist[end];
     }
 };
+
+
+class Solution {
+public:
+    void shortestDistance(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == -1) matrix[i][j] = 1e9;
+                if(i == j) matrix[i][j] = 0;
+            }
+        }
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; k++){
+                for(int j = 0; j < n; j++){
+                    if(matrix[i][k] < 1e9 && matrix[k][j] < 1e9){
+                        matrix[i][j] = min(matrix[i][j],  matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }for(int i = 0; i < n; i++){
+            if(matrix[i][] == 0){
+                cout << "Negative cycle exist" < "\n";
+            }
+        }for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] >= 1e9) matrix[i][j] = -1;
+            }
+        }
+    }
+};
