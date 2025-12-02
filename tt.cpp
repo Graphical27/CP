2239e735e96c982062d7d9e55883688cc33bae87
Sourabh Singh <specious0027@gmail.com>
2025-12-02 11:37:28 +0530

    Updated

--- tt.cpp
+++ tt.cpp
@@ -1338,3 +1338,30 @@ public:
         return maxi == 1e9 ? -1 : maxi;
     }
 };
+
+
+class Solution {
+    public:
+        vector<int> shortestPath(vector<vector<int>>& edges, int N, int M){
+            vector<pair<int,int>> adj[N];
+            for(auto e:edges){
+                adj[e[0]].push_back({e[1],e[2]});
+                adj[e[1]].push_back({e[0],e[2]});
+            }
+            vector<int> dist(N,1e9);
+            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+            pq.push({0,0});
+            dist[0] = 0;
+            while(!pq.empty()){
+                auto[d,u] = pq.top();
+                pq.pop();
+                if(d > dist[u]) continue;
+                for(auto [v,w]:adj[u]){
+                    if(dist[v] > dist[u] + w){
+                        dist[v] = dist[u] + w;
+                    }
+                }
+            }for(auto x:dist) x = x == 1e9 ? -1 : x;
+            return dist;
+        }
+};
