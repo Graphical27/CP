cf795609764714cb93d324dd02da59a6d6ff0913
Sourabh Singh <specious0027@gmail.com>
2025-12-03 21:54:51 +0530

    updated

--- tt.cpp
+++ tt.cpp
@@ -1365,3 +1365,31 @@ class Solution {
             return dist;
         }
 };
+
+
+class Solution {
+public:
+    void shortestDistance(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        const int inf  = 1e9;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == -1) matrix[i][j] = inf;
+                if(i == j) matrix[i][j] = 1;
+            }
+        }
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(matrix[i][k] != 1e9 && matrix[k][j] != 1e9){
+                        matrix[i][j] = min(matrix[i][j],matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }for(int i = 0; i < n; i++){
+            if(matrix[i][j] >= 1e9){
+                cout << " Circle Found\n";
+            }
+        }
+    }
+};
